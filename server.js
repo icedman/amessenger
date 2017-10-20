aa5a27ad3ac09d977ab6a032fccffd6c3df9498e
Marvin Sanchez <m4rvin2005@gmail.com>
2017-10-20 22:48:30 +0800

    add redis pub/sub

--- server.js
+++ server.js
@@ -2,16 +2,42 @@
 
 var app = require('./index');
 var http = require('http');
+var moment = require('moment');
 
+var redis = require('ioredis');
+// var redis_address = process.env.REDIS_ADDRESS || 'redis://127.0.0.1:6379';
+var sub = redis.createClient(), pub = redis.createClient();
+var channel_history_max = 10;
+
+var room = 'global';
+var room_messages = 'message_' + room;
 var server;
 
 /*
  * Create and start HTTP server.
  */
-
 server = http.createServer(app);
 
 var io = require('socket.io')(server);
+
+sub.on('subscribe', function (channel, count) {
+    console.log(channel + ': ' + count);
+});
+
+sub.on('message', function (channel, message) {
+    var msg = JSON.parse(message);
+    var date = moment.now();
+    
+    pub.zadd(room_messages, date, message);
+
+    if ((msg.to || '').length > 0) {
+        io.in(channel).emit('messages', msg);
+    } else {
+        io.emit('messages', msg);
+    }
+});
+
+sub.subscribe(room);
 
 server.listen(process.env.PORT || 8000);
 server.on('listening', function () {
@@ -20,24 +46,54 @@ server.on('listening', function () {
 
 io.on('connection', function(socket) {
     
+    var _username = 'anonymous';
+    
     console.log('connected...');
-
     socket.on('send', function(message) {
-
-        console.log('send received..' + message);
-
-        if (message.to !== undefined && message.to !== '') {
-            io.to(message.to).emit('messages', 'echo ' + message.message);
+        if ((message.to || '').length > 0) {
+            pub.publish(message.to, JSON.stringify( { from: _username, to: message.to, message: message.message } ));
         } else {
-            io.emit('messages', 'echo ' + message.message);
+            pub.publish(room, JSON.stringify( { from: _username, to: message.to, message: message.message } ));
         }
-        
+    });
+
+    socket.on('disconnect', function() {
+        sub.unsubscribe(_username);
+        io.emit('messages', {message:_username + ' has left'});
+        // pub.publish(room, JSON.stringify( { message: 'disconnected ..' + _username } ));
     });
 
     socket.on('join', function(username) {
 
-        socket.join(username);
-        io.in(username).emit('messages', 'welcome ' + username);
+        _username = username;
+        socket.join(_username);
+        sub.subscribe(_username);
+
+        var get_messages = pub.zrange(room_messages, -1 * channel_history_max, -1)
+        .then(function(result) {
+            return result.map(function(x) {
+                return JSON.parse(x);
+            });
+        });
         
+        Promise.all([get_messages]).then(function(messages) {
+            var msgs = messages[0];
+            msgs.forEach(function(m) {
+                if ((m.from || '').length > 0 && (m.to === _username || m.to === '')) {
+                    // pub.publish(_username, JSON.stringify(m));
+                    io.in(_username).emit('messages', m);
+                }
+            });
+
+            io.emit('messages', {message:_username + ' has joined'});
+            io.in(_username).emit('messages', {message:'Welcome, ' + _username});
+
+            // pub.publish(_username, JSON.stringify({ to:_username, message:'Welcome, ' + _username}));
+        })
+        .catch(function(e) {
+            console.log('error:' + e);
+        });
+
     });
 });
+
