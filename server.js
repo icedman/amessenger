63cd2915bad98ff586637623599e0835eaab9cdf
Marvin Sanchez <m4rvin2005@gmail.com>
2017-11-05 14:04:24 +0800

    refactor

--- server.js
+++ server.js
@@ -2,113 +2,16 @@
 
 var app = require('./index');
 var http = require('http');
-var moment = require('moment');
-
-var redis = require('ioredis');
-// var redis_address = process.env.REDIS_ADDRESS || 'redis://127.0.0.1:6379';
-var sub = redis.createClient(), pub = redis.createClient();
-var channel_history_max = 10;
-
-var room = 'global';
-var server;
+var dispatcher = require('./lib/dispatcher').Dispatcher;
 
 /*
  * Create and start HTTP server.
  */
-server = http.createServer(app);
+var server = http.createServer(app);
 
-var io = require('socket.io')(server);
-
-sub.on('message', function (channel, message) {
-
-    console.log(channel);
-
-    var msg = JSON.parse(message);
-    var date = moment.now();
-    
-    pub.zadd('message_' + room, date, message);
-
-    if ((msg.to || '').length > 0) {
-        io.in(channel).emit('messages', msg);
-    } else {
-        io.emit('messages', msg);
-    }
-});
-
-sub.subscribe(room, function(err, count) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log(room + ': ' + count);
-});
+dispatcher.initialize(server);
 
 server.listen(process.env.PORT || 8000);
 server.on('listening', function () {
     console.log('Server listening on http://localhost:%d', this.address().port);
 });
-
-io.on('connection', function(socket) {
-    
-    var _username = 'anonymous';
-    
-    console.log('connected...');
-    socket.on('send', function(message) {
-
-        console.log('send.. ' + _username);
-        
-        if ((message.to || '').length > 0) {
-            pub.publish(message.to, JSON.stringify( { from: _username, to: message.to, message: message.message } ));
-            // pub.publish(_username, JSON.stringify( { from: _username, to: message.to, message: message.message } ));
-        } else {
-            pub.publish(room, JSON.stringify( { from: _username, to: message.to, message: message.message } ));
-        }
-    });
-
-    socket.on('disconnect', function() {
-        sub.unsubscribe(_username);
-        // pub.publish(room, JSON.stringify( { message: 'disconnected ..' + _username } ));
-    });
-
-    socket.on('join', function(username) {
-
-        _username = username;
-        socket.join(_username);
-
-        sub.subscribe(_username, function(err, count) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log(_username + ': ' + count);
-        });
-
-        var get_messages = pub.zrange('message_' + room, -1 * channel_history_max, -1)
-        .then(function(result) {
-            return result.map(function(x) {
-                return JSON.parse(x);
-            });
-        });
-        
-        Promise.all([get_messages]).then(function(messages) {
-            var msgs = messages[0];
-            msgs.forEach(function(m) {
-                if ((m.from || '').length > 0 && (m.to === _username || m.to === '')) {
-                    // pub.publish(_username, JSON.stringify(m));
-                    io.in(_username).emit('messages', m);
-                }
-            });
-
-            // io.emit('messages', {message:_username + ' has joined'});
-            pub.publish(room, JSON.stringify( {message:_username + ' has joined'} ));
-
-            io.in(_username).emit('messages', {message:'Welcome, ' + _username});
-            // pub.publish(_username, JSON.stringify({ to:_username, message:'Welcome, ' + _username}));
-        })
-        .catch(function(e) {
-            console.log('error:' + e);
-        });
-
-    });
-});
-
