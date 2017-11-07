31ca4ff582b451b55716213e17da50c2c66e2f8b
Marvin Sanchez <m4rvin2005@gmail.com>
2017-11-07 12:43:34 +0800

    add poker game room

--- rooms/poker.js
+++ rooms/poker.js
@@ -27,15 +27,30 @@ class Poker {
                 }
 
                 self.game.ping(message);
+                self.game.dump(true);
 
-                console.log(JSON.stringify(self.game.state, null, 4));
-                console.log(message.command);
+                // console.log(JSON.stringify(self.game.state, null, 4));
+                // console.log(message.command);
                 // console.log('state: ' + JSON.stringify(self.game.state));
 
                 // save updated state
                 client.set(stateKey, JSON.stringify(self.game.state));
 
                 // allow dispatcher.. or self-publish
+                if (message.ping != undefined) {
+                    setTimeout( function() {
+                        console.log('ping requested');
+                        self.game.ping( { command: [ 0, 'ping' ]});
+                    }, message.ping);
+                }
+
+                // frame reply
+                message.from = 'poker';
+                message.message = self.game.state.state;
+                if (message.result != undefined) {
+                    message.message += ':' + message.result;
+                }
+
                 resolve(message);
             });
         });
