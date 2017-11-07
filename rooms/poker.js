79478e83ee6f0312069d5eb286a5ec5d803a2cb0
Marvin Sanchez <m4rvin2005@gmail.com>
2017-11-07 21:48:11 +0800

    poker room edits

--- rooms/poker.js
+++ rooms/poker.js
@@ -1,49 +1,113 @@
 'use strict';
 
-var PokerGame = require('../../pokerworld/PokerGame').PokerGame;
+// let PokerGame = require('../../pokerworld/PokerGame').PokerGame;
+let PokerGame = require('../../pokerworld/game/Game').PokerGame;
 
 class Poker {
 
     constructor() {
         console.log('new Poker');
         this.game = new PokerGame();
+        this.pingRequests = [];
+
+        setInterval( ()=> {
+            
+            // console.log('.' + this.game._state + this.pingRequests.length);
+
+            let self = this;
+            this.pingRequests.forEach( (req) => {
+                setTimeout( function() {
+                    console.log('do as requested');
+                    
+                    req.message.reply = { messages: [] };
+                    self.pipe(req.message, req.pubsub)
+                    .then( function(message) {
+
+                        // broadcast!
+                        message.reply.messages.forEach( (m) => {
+                            m.channel = req.message.channel;
+                            req.pubsub.publish(m); }
+                        );
+
+                    })
+                    .catch( ()=> {
+                        // drop!
+                    });
+
+                }, req.timeout);
+            });
+
+            this.pingRequests = [];
+            
+
+        }, 2000);
+    }
+
+    _pingRequest(message, pubsub, timeout) {
+
+        let isPendingRequest = false;
+        this.pingRequests.map( (r)=>{ 
+            return r.pubsub;
+        }).forEach( (p) => {
+            if (p === pubsub) {
+                isPendingRequest = true;
+            }
+        });
+
+        if (isPendingRequest) {
+            return;
+        }
+
+        this.pingRequests.push({
+            internal: true,
+            message: message,
+            pubsub: pubsub,
+            timeout: timeout
+        });
     }
 
     pipe(message, pubsub) {
         console.log('poker pipe: ' + JSON.stringify(message));
 
-        var self = this;
+        let self = this;
 
         return new Promise( function(resolve) {
-            var stateKey = 'state::' + message.channel;
-            var client = pubsub.client();
+            let stateKey = 'state::' + message.channel;
+            let client = pubsub.client();
 
             client.get(stateKey, function(err, result) {
+
+                let game= self.game;
 
                 let state;
                 try {
                     state = JSON.parse(result);
-                    self.game.state = state;
+                    game.state = state;
                 } catch(e) {
-                    self.game.createGame();
+                    game.createGame();
                 }
 
-                message.reply = self.game.ping(message);
-                // self.game.dump(true);
+                message.reply = game.ping(message);
+                // game.dump(true);
 
                 console.log(message.reply);
 
                 // save updated state
-                client.set(stateKey, JSON.stringify(self.game.state));
+                client.set(stateKey, JSON.stringify(game.state));
                 client.expire(stateKey, 60*5); // expire within 5 minutes
 
-                // allow dispatcher.. or self-publish
-                if (self.game.state.ping > 0) {
-                    console.log('ping requested');
-                    setTimeout( function() {
-                        console.log('do ping');
-                        self.game.ping( { command: [ 0, 'ping' ]});
-                    }, self.game.state.ping);
+                if (game.state.ping > 0) {
+                    self._pingRequest({ 
+                        from:'poker',
+                        to:message.channel,
+                        channel:message.channel,
+                        command: [ 0, 'ping' ]
+                    }, pubsub, game.state.ping);
+                }
+
+                if (message.internal != undefined) {
+                    delete message.to;
+                    delete message.channel;
                 }
 
                 resolve(message);
